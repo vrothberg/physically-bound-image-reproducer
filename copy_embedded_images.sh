9d30a34dda361f99d39f4d9e56fd2fe5ab8a5b16
Valentin Rothberg <vrothberg@redhat.com>
2025-03-18 16:25:14 +0100

    use /usr/lib instead of /opt
    Fixes: #1
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

--- copy_embedded_images.sh
+++ copy_embedded_images.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 
 while IFS="," read -r image sha
 do
-	skopeo copy --preserve-digests dir:/opt/image-cache/$sha containers-storage:$image
-done < /opt/image-cache/mapping.txt
+	skopeo copy --preserve-digests dir:/usr/lib/containers-image-cache/$sha containers-storage:$image
+done < /usr/lib/containers-image-cache/mapping.txt
--- embed_image.sh
+++ embed_image.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 image=$1
 additional_copy_args=${2:-""}
 
-mkdir -p /opt/image-cache
+mkdir -p /usr/lib/containers-image-cache
 sha=$(echo "$image" | sha256sum | awk '{ print $1 }')
-skopeo copy $additional_copy_args --preserve-digests docker://$image dir:/opt/image-cache/$sha
-echo "$image,$sha" >> /opt/image-cache/mapping.txt
+skopeo copy $additional_copy_args --preserve-digests docker://$image dir:/usr/lib/containers-image-cache/$sha
+echo "$image,$sha" >> /usr/lib/containers-image-cache/mapping.txt
